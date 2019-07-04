4d68ba9d24f56864d2ffd76524c39e9f2e654aeb
mitoma <mutetheradio@gmail.com>
2019-07-05 00:14:49 +0900

    SmoothValue の実装

--- src/smooth_value.rs
+++ src/smooth_value.rs
@@ -1,10 +1,64 @@
 use std::collections::*;
+use std::f64::*;
 
 pub enum MovingType {
     Liner,
     Smooth,
     SmoothIn,
     SmoothOut,
+    Bound,
+}
+
+impl MovingType {
+    fn gains(&mut self, num_of_div: i16) -> Vec<f64> {
+        let mut vec = Vec::new();
+        match self {
+            MovingType::Liner => {
+                let gain = 1.0 / num_of_div as f64;
+                for _ in 0..num_of_div {
+                    vec.push(gain)
+                }
+            }
+            MovingType::Smooth => {
+                let gain = 1.0 / num_of_div as f64;
+                let g = consts::PI * gain;
+                for i in 0..num_of_div {
+                    let i = i as f64;
+                    let div_gain = (g + i).cos() - (g * (i + 1.0)).cos();
+                    vec.push(div_gain / 2.0)
+                }
+            }
+            MovingType::SmoothOut => {
+                let gain = 1.0 / num_of_div as f64;
+                let g = consts::PI * gain / 2.0;
+                for i in 0..num_of_div {
+                    let div_gain = (g * (i as f64 + 1.0)).sin() - (g * i as f64).sin();
+                    vec.push(div_gain)
+                }
+            }
+            MovingType::SmoothIn => {
+                let gain = 1.0 / num_of_div as f64;
+                let g = consts::PI * gain / 2.0;
+                let start = consts::PI * 1.5;
+                for i in 0..num_of_div {
+                    let div_gain =
+                        (start + (g * (i as f64 + 1.0))).sin() - (start + (g * i as f64)).sin();
+                    vec.push(div_gain)
+                }
+            }
+            MovingType::Bound => {
+                let gain = 1.0 / num_of_div as f64;
+                let g = consts::PI * 1.5 * gain;
+                let qg = consts::PI * 4.0;
+                let dd = qg.sin() * 2.0;
+                for i in 0..num_of_div {
+                    let div_gain = ((g * i as f64) + qg).sin() - (g * (i as f64 + 1.0) + qg).sin();
+                    vec.push(div_gain / dd)
+                }
+            }
+        }
+        vec
+    }
 }
 
 struct SmoothValue {
@@ -31,11 +85,30 @@ impl SmoothValue {
         }
     }
 
-    pub fn next(&mut self) -> &SmoothValue {
-        if let Some(next_value) = self.queue.pop_front() {
-            self.current_value = next_value;
+    pub fn next(&mut self) -> f64 {
+        if let Some(gain_value) = self.queue.pop_front() {
+            self.current_value += gain_value;
         }
-        self
+        self.current_value
+    }
+
+    pub fn update(&mut self, next_value: f64) {
+        if self.value == next_value {
+            return;
+        }
+        let mut new_queue: VecDeque<f64> = VecDeque::new();
+        let delta = next_value - self.value;
+        self.value = next_value;
+
+        let gains = self.moving_type.gains(self.num_of_div);
+        gains.iter().for_each(|gain| {
+            let mut value = gain * delta;
+            if let Some(v) = self.queue.pop_front() {
+                value += v;
+            }
+            new_queue.push_back(value)
+        });
+        self.queue = new_queue
     }
 }
 
@@ -46,8 +119,24 @@ mod tests {
 
     #[test]
     fn liner() {
-        let value = SmoothValue::new(100.0, 100.0, MovingType::Liner, 15);
-        value.current_value;
+        let mut value = SmoothValue::new(100.0, 100.0, MovingType::Liner, 5);
+        value.update(200.0);
+        assert_eq!(value.next(), 120.0);
+        assert_eq!(value.next(), 140.0);
+        assert_eq!(value.next(), 160.0);
+        assert_eq!(value.next(), 180.0);
+        assert_eq!(value.next(), 200.0);
+        assert_eq!(value.next(), 200.0);
+        value.update(100.0);
+        assert_eq!(value.next(), 180.0);
+        assert_eq!(value.next(), 160.0);
+        value.update(0.0);
+        assert_eq!(value.next(), 120.0);
+        assert_eq!(value.next(), 80.0);
+        assert_eq!(value.next(), 40.0);
+        assert_eq!(value.next(), 20.0);
+        assert_eq!(value.next(), 0.0);
+        assert_eq!(value.next(), 0.0);
     }
 
 }
