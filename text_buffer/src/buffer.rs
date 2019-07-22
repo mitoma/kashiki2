ba99c46f04baf2e4fb97e53570321726dca183b5
mitoma <mutetheradio@gmail.com>
2019-07-23 06:52:23 +0900

    実装とテストを書いていった

--- text_buffer/src/buffer.rs
+++ text_buffer/src/buffer.rs
@@ -1,3 +1,8 @@
+pub struct Caret {
+    row: usize,
+    col: usize,
+}
+
 pub struct Buffer {
     buffer_name: String,
     lines: Vec<BufferLine>,
@@ -5,9 +10,30 @@ pub struct Buffer {
 
 impl Buffer {
     pub fn new(buffer_name: String) -> Buffer {
+        let mut lines = Vec::new();
+        let line = BufferLine::new();
+        lines.push(line);
         Buffer {
             buffer_name: buffer_name,
-            lines: Vec::new(),
+            lines: lines,
+        }
+    }
+
+    pub fn to_buffer_string(&self) -> String {
+        self.lines
+            .iter()
+            .map(|l| l.to_line_string())
+            .collect::<Vec<String>>()
+            .join("\n")
+    }
+
+    pub fn insert_char(&mut self, mut caret: Caret, c: char) {
+        match self.lines.get_mut(caret.row) {
+            Some(line) => {
+                line.insert_char(caret.col, c);
+                caret.col +=1
+            }
+            None => {}
         }
     }
 }
@@ -22,6 +48,13 @@ impl BufferLine {
         BufferLine {
             row_num: 0,
             chars: Vec::new(),
+        }
+    }
+
+    pub fn from_chars(chars: Vec<BufferChar>) -> BufferLine {
+        BufferLine {
+            row_num: 0,
+            chars: chars,
         }
     }
 
@@ -45,8 +78,16 @@ impl BufferLine {
             .insert(col, BufferChar::new(col, self.row_num, c))
     }
 
+    pub fn insert_enter(&mut self, col: usize) -> BufferLine {
+        BufferLine::from_chars(self.chars.split_off(col))
+    }
+
     pub fn remove_char(&mut self, col: usize) {
         self.chars.remove(col);
+    }
+
+    pub fn join(&mut self, line: BufferLine) {
+        line.chars.into_iter().for_each({ |c| self.chars.push(c) })
     }
 }
 
@@ -77,7 +118,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn buffer_line() {
+    fn buffer() {
+        let mut sut = Buffer::new("hello buffer".to_string());
+        assert_eq!(sut.to_buffer_string(), "");
+    }
+
+    #[test]
+    fn buffer_line_insert_remove() {
         let mut sut = BufferLine::new();
         assert_eq!(sut.to_line_string(), "");
         sut.insert_char(0, '鉄');
@@ -92,4 +139,20 @@ mod tests {
         sut.remove_char(3);
         assert_eq!(sut.to_line_string(), "鉄鍋のン");
     }
+
+    #[test]
+    fn buffer_line_enter_join() {
+        let mut sut = BufferLine::new();
+        assert_eq!(sut.to_line_string(), "");
+        sut.insert_char(0, '花');
+        sut.insert_char(1, '鳥');
+        sut.insert_char(2, '風');
+        sut.insert_char(3, '月');
+        let sut2 = sut.insert_enter(2);
+        assert_eq!(sut.to_line_string(), "花鳥");
+        assert_eq!(sut2.to_line_string(), "風月");
+        sut.join(sut2);
+        assert_eq!(sut.to_line_string(), "花鳥風月");
+    }
+
 }
