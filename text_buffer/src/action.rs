4fcc373f996d30766f17d1e27239801ea63ab9ff
mitoma <mutetheradio@gmail.com>
2019-08-16 22:39:01 +0900

    ApplyResult という構造体にまとめることにした

--- text_buffer/src/action.rs
+++ text_buffer/src/action.rs
@@ -23,17 +23,23 @@ pub struct ReverseAction {
     actions: Vec<BufferAction>,
 }
 
+pub struct ApplyResult {
+    buffer: Buffer,
+    caret: Caret,
+    reverse_action: ReverseAction,
+}
+
 pub fn apply_reserve_actions(buffer: Buffer, reverse_action: &ReverseAction) -> (Buffer, Caret) {
     reverse_action
         .actions
         .iter()
         .fold((buffer, Caret::new(0, 0)), |(buffer, _), action| {
             let result = apply_action(buffer, &action);
-            (result.0, result.1)
+            (result.buffer, result.caret)
         })
 }
 
-pub fn apply_action(mut buffer: Buffer, action: &BufferAction) -> (Buffer, Caret, ReverseAction) {
+pub fn apply_action(mut buffer: Buffer, action: &BufferAction) -> ApplyResult {
     let mut result = ReverseAction {
         actions: Vec::new(),
     };
@@ -42,48 +48,83 @@ pub fn apply_action(mut buffer: Buffer, action: &BufferAction) -> (Buffer, Caret
         BufferAction::MoveTo(pre_caret) => {
             // MoveTo には from と to 二つ必要かどうかよくわかっていない。
             result.actions.push(BufferAction::MoveTo(pre_caret.clone()));
-            (buffer, pre_caret.clone(), result)
+            ApplyResult {
+                buffer: buffer,
+                caret: pre_caret.clone(),
+                reverse_action: result,
+            }
         }
-
         BufferAction::Head(pre_caret) => {
             result.actions.push(BufferAction::MoveTo(pre_caret.clone()));
             let post_caret = buffer.head(pre_caret.clone());
-            (buffer, post_caret, result)
+            ApplyResult {
+                buffer: buffer,
+                caret: post_caret,
+                reverse_action: result,
+            }
         }
         BufferAction::Last(pre_caret) => {
             result.actions.push(BufferAction::MoveTo(pre_caret.clone()));
             let post_caret = buffer.last(pre_caret.clone());
-            (buffer, post_caret, result)
+            ApplyResult {
+                buffer: buffer,
+                caret: post_caret,
+                reverse_action: result,
+            }
         }
         BufferAction::Back(pre_caret) => {
             result.actions.push(BufferAction::MoveTo(pre_caret.clone()));
             let post_caret = buffer.back(pre_caret.clone());
-            (buffer, post_caret, result)
+            ApplyResult {
+                buffer: buffer,
+                caret: post_caret,
+                reverse_action: result,
+            }
         }
         BufferAction::Forward(pre_caret) => {
             result.actions.push(BufferAction::MoveTo(pre_caret.clone()));
             let post_caret = buffer.forward(pre_caret.clone());
-            (buffer, post_caret, result)
+            ApplyResult {
+                buffer: buffer,
+                caret: post_caret,
+                reverse_action: result,
+            }
         }
         BufferAction::Previous(pre_caret) => {
             result.actions.push(BufferAction::MoveTo(pre_caret.clone()));
             let post_caret = buffer.previous(pre_caret.clone());
-            (buffer, post_caret, result)
+            ApplyResult {
+                buffer: buffer,
+                caret: post_caret,
+                reverse_action: result,
+            }
         }
         BufferAction::Next(pre_caret) => {
             result.actions.push(BufferAction::MoveTo(pre_caret.clone()));
             let post_caret = buffer.next(pre_caret.clone());
-            (buffer, post_caret, result)
+            ApplyResult {
+                buffer: buffer,
+                caret: post_caret,
+                reverse_action: result,
+            }
         }
         BufferAction::BufferHead(pre_caret) => {
             result.actions.push(BufferAction::MoveTo(pre_caret.clone()));
             let post_caret = buffer.buffer_head(pre_caret.clone());
-            (buffer, post_caret, result)
+            ApplyResult {
+                buffer: buffer,
+                caret: post_caret,
+                reverse_action: result,
+            }
         }
         BufferAction::BufferLast(pre_caret) => {
             result.actions.push(BufferAction::MoveTo(pre_caret.clone()));
             let post_caret = buffer.buffer_last(pre_caret.clone());
-            (buffer, post_caret, result)
+            ApplyResult {
+                buffer: buffer,
+                caret: post_caret,
+                reverse_action: result,
+            }
         }
 
         BufferAction::InsertEnter(pre_caret) => {
@@ -91,14 +132,22 @@ pub fn apply_action(mut buffer: Buffer, action: &BufferAction) -> (Buffer, Caret
             result
                 .actions
                 .push(BufferAction::Backspace(post_caret.clone()));
-            (buffer, post_caret, result)
+            ApplyResult {
+                buffer: buffer,
+                caret: post_caret,
+                reverse_action: result,
+            }
         }
         BufferAction::InsertChar(pre_caret, char_value) => {
             let post_caret = buffer.insert_char(pre_caret.clone(), char_value.clone());
             result
                 .actions
                 .push(BufferAction::Backspace(post_caret.clone()));
-            (buffer, post_caret, result)
+            ApplyResult {
+                buffer: buffer,
+                caret: post_caret,
+                reverse_action: result,
+            }
         }
         BufferAction::InsertString(pre_caret, str_value) => {
             result.actions.push(BufferAction::MoveTo(pre_caret.clone()));
@@ -106,7 +155,11 @@ pub fn apply_action(mut buffer: Buffer, action: &BufferAction) -> (Buffer, Caret
             for _ in 0..str_value.chars().count() {
                 result.actions.push(BufferAction::Delete(pre_caret.clone()));
             }
-            (buffer, post_caret, result)
+            ApplyResult {
+                buffer: buffer,
+                caret: post_caret,
+                reverse_action: result,
+            }
         }
         BufferAction::Backspace(pre_caret) => {
             let (post_caret, removed_char) = buffer.backspace(pre_caret.clone());
@@ -119,7 +172,11 @@ pub fn apply_action(mut buffer: Buffer, action: &BufferAction) -> (Buffer, Caret
                     .push(BufferAction::InsertEnter(post_caret.clone())),
                 RemovedChar::None => {}
             }
-            (buffer, post_caret, result)
+            ApplyResult {
+                buffer: buffer,
+                caret: post_caret,
+                reverse_action: result,
+            }
         }
         BufferAction::Delete(pre_caret) => {
             let removed_char = buffer.delete(pre_caret);
@@ -138,7 +195,11 @@ pub fn apply_action(mut buffer: Buffer, action: &BufferAction) -> (Buffer, Caret
                 }
                 RemovedChar::None => {}
             }
-            (buffer, pre_caret.clone(), result)
+            ApplyResult {
+                buffer: buffer,
+                caret: pre_caret.clone(),
+                reverse_action: result,
+            }
         }
     }
 }
@@ -151,9 +212,9 @@ mod tests {
     fn test_buffer_move() {
         let mut sut = Buffer::new("hello buffer".to_string());
         sut.insert_string(Caret::new(0, 0), "ABCD\nEFGH\nIJKL\nMNO".to_string());
-        let (sut, caret, action) = apply_action(sut, &BufferAction::Last(Caret::new(0, 0)));
-        assert_eq!(caret, Caret::new(0, 4));
-        let (_, caret) = apply_reserve_actions(sut, &action);
+        let result = apply_action(sut, &BufferAction::Last(Caret::new(0, 0)));
+        assert_eq!(result.caret, Caret::new(0, 4));
+        let (_, caret) = apply_reserve_actions(result.buffer, &result.reverse_action);
         assert_eq!(caret, Caret::new(0, 0));
     }
 
@@ -161,33 +222,38 @@ mod tests {
     fn test_apply_action() {
         let sut = Buffer::new("hello buffer".to_string());
         let mut reverses = Vec::new();
-        let (sut, caret, action) =
-            apply_action(sut, &BufferAction::InsertChar(Caret::new(0, 0), '花'));
-        reverses.push(action);
-        let (sut, caret, action) = apply_action(sut, &BufferAction::InsertChar(caret, '鳥'));
-        reverses.push(action);
-        let (sut, _, action) = apply_action(sut, &BufferAction::InsertChar(caret, '\n'));
-        reverses.push(action);
-        assert_eq!(sut.to_buffer_string(), "花鳥\n".to_string());
+        let result = apply_action(sut, &BufferAction::InsertChar(Caret::new(0, 0), '花'));
+        reverses.push(result.reverse_action);
+        let result = apply_action(
+            result.buffer,
+            &BufferAction::InsertChar(result.caret, '鳥'),
+        );
+        reverses.push(result.reverse_action);
+        let result = apply_action(result.buffer, &BufferAction::InsertChar(result.caret, '\n'));
+        reverses.push(result.reverse_action);
+        assert_eq!(result.buffer.to_buffer_string(), "花鳥\n".to_string());
 
         let reverse_action = reverses.pop().unwrap();
-        let sut = reverse_action.actions.iter().fold(sut, |sut, action| {
-            let (sut, _, _) = apply_action(sut, action);
-            sut
-        });
+        let sut = reverse_action
+            .actions
+            .iter()
+            .fold(result.buffer, |buffer, action| {
+                let result = apply_action(buffer, action);
+                result.buffer
+            });
         assert_eq!(sut.to_buffer_string(), "花鳥".to_string());
 
         let reverse_action = reverses.pop().unwrap();
-        let sut = reverse_action.actions.iter().fold(sut, |sut, action| {
-            let (sut, _, _) = apply_action(sut, action);
-            sut
+        let sut = reverse_action.actions.iter().fold(sut, |buffer, action| {
+            let result = apply_action(buffer, action);
+            result.buffer
         });
         assert_eq!(sut.to_buffer_string(), "花".to_string());
 
         let reverse_action = reverses.pop().unwrap();
-        let sut = reverse_action.actions.iter().fold(sut, |sut, action| {
-            let (sut, _, _) = apply_action(sut, action);
-            sut
+        let sut = reverse_action.actions.iter().fold(sut, |buffer, action| {
+            let result = apply_action(buffer, action);
+            result.buffer
         });
         assert_eq!(sut.to_buffer_string(), "".to_string());
     }
