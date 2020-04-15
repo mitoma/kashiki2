fe966d01ab1a6d6f70f2481dee2bf6571d9237ee
mitoma <mutetheradio@gmail.com>
2020-04-15 17:11:45 +0900

    ひとまず雑なコードを足してみる

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -11,6 +11,7 @@ fn main() {
 
     let mut caret = text_buffer::caret::Caret::new(0, 0);
     let mut text_buffer = text_buffer::buffer::Buffer::new();
+    caret = text_buffer.insert_string(caret, "hello world".to_string());
 
     let mut glyphs = Glyphs::new(
         "asset/TakaoGothic.ttf",
@@ -32,6 +33,27 @@ fn main() {
 
         if let Some(action) = input_action {
             println!("input:{:?}", action);
+            match action {
+                InputAction::TextAction(s) => caret = text_buffer.insert_string(caret, s),
+                InputAction::KeyAction(key_with_meta) => {
+                    match key_with_meta {
+                        KeyWithMeta {
+                            key: gui::key_adapter::Key::Backspace,
+                            meta_key: MetaKey::None,
+                        } => {
+                            let moge = text_buffer.backspace(caret);
+                            caret = moge.0
+                        }
+                        KeyWithMeta {
+                            key: gui::key_adapter::Key::Return,
+                            meta_key: MetaKey::None,
+                        } => {
+                            caret = text_buffer.insert_enter(caret);
+                        }
+                        _ => {}
+                    };
+                }
+            }
         }
 
         if let Some(_args) = event.render_args() {
