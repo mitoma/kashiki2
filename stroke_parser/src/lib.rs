78661666ecf967f678df4043d925ff1413ffd96f
mitoma <mutetheradio@gmail.com>
2020-04-27 09:51:01 +0900

    実装を整理

--- stroke_parser/src/lib.rs
+++ stroke_parser/src/lib.rs
@@ -1,9 +1,8 @@
-use std::collections::HashMap;
 use winit::event::{
     ElementState, Event, KeyboardInput, ModifiersState, VirtualKeyCode, WindowEvent,
 };
 
-#[derive(Debug, Hash, Ord, PartialOrd, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, Hash, Ord, PartialOrd, PartialEq, Eq, Copy, Clone)]
 pub struct KeyWithModifier {
     key: VirtualKeyCode,
     modifires: ModifiersState,
@@ -35,49 +34,69 @@ pub struct Stroke {
     keys: Vec<KeyWithModifier>,
 }
 
+impl Default for Stroke {
+    fn default() -> Self {
+        Stroke { keys: Vec::new() }
+    }
+}
+
+impl Stroke {
+    fn append_key(&mut self, key: KeyWithModifier) {
+        self.keys.push(key)
+    }
+
+    fn starts_with(&self, stroke: &Stroke) -> bool {
+        self.keys.starts_with(&stroke.keys)
+    }
+
+    fn clear(&mut self) {
+        self.keys.clear()
+    }
+}
+
+#[derive(Debug, Hash, Ord, PartialOrd, PartialEq, Eq, Clone)]
 pub struct KeyBind {
     stroke: Stroke,
     action: Action,
 }
 
 pub struct ActionStore {
-    keybinds: HashMap<Stroke, Action>,
+    keybinds: Vec<KeyBind>,
     current_modifier: ModifiersState,
+    current_stroke: Stroke,
 }
 
 impl Default for ActionStore {
     fn default() -> Self {
         let mut store = ActionStore {
-            keybinds: HashMap::new(),
+            keybinds: Vec::new(),
             current_modifier: ModifiersState::empty(),
+            current_stroke: Default::default(),
         };
-        store.keybinds.insert(
-            Stroke {
+        store.keybinds.push(KeyBind {
+            stroke: Stroke {
                 keys: vec![KeyWithModifier {
                     key: VirtualKeyCode::Escape,
                     modifires: ModifiersState::empty(),
                 }],
             },
-            Action::new("exit", ActionCategory::System),
-        );
-        store.keybinds.insert(
-            Stroke {
-                keys: vec![KeyWithModifier {
-                    key: VirtualKeyCode::C,
-                    modifires: ModifiersState::CTRL,
-                }],
+            action: Action::new("exit", ActionCategory::System),
+        });
+        store.keybinds.push(KeyBind {
+            stroke: Stroke {
+                keys: vec![
+                    KeyWithModifier {
+                        key: VirtualKeyCode::X,
+                        modifires: ModifiersState::CTRL,
+                    },
+                    KeyWithModifier {
+                        key: VirtualKeyCode::C,
+                        modifires: ModifiersState::CTRL,
+                    },
+                ],
             },
-            Action::new("exit", ActionCategory::System),
-        );
-        store.keybinds.insert(
-            Stroke {
-                keys: vec![KeyWithModifier {
-                    key: VirtualKeyCode::P,
-                    modifires: ModifiersState::SHIFT,
-                }],
-            },
-            Action::new("exit", ActionCategory::System),
-        );
+            action: Action::new("exit", ActionCategory::System),
+        });
         store
     }
 }
@@ -98,13 +117,22 @@ impl ActionStore {
                     },
                 ..
             } => {
-                if let Some(action) = self.keybinds.get(&Stroke {
-                    keys: vec![KeyWithModifier {
-                        key: *keycode,
-                        modifires: self.current_modifier.clone(),
-                    }],
-                }) {
+                if Self::is_modifire_key(keycode) {
+                    return None;
+                }
+
+                self.current_stroke.append_key(KeyWithModifier {
+                    key: *keycode,
+                    modifires: self.current_modifier.clone(),
+                });
+
+                if let Some(action) = self.get_action() {
+                    self.current_stroke.clear();
                     return Some(action.clone());
+                }
+
+                if !self.in_stroke() {
+                    self.current_stroke.clear();
                 }
                 None
             }
@@ -115,8 +143,36 @@ impl ActionStore {
                 self.current_modifier = state.clone();
                 None
             }
-
             _ => None,
+        }
+    }
+
+    fn get_action(&self) -> Option<Action> {
+        self.keybinds
+            .iter()
+            .find(|keybind| keybind.stroke == self.current_stroke)
+            .map(|keybind| keybind.action.clone())
+    }
+
+    fn in_stroke(&self) -> bool {
+        for KeyBind { stroke, .. } in &self.keybinds {
+            if stroke.starts_with(&self.current_stroke) {
+                return true;
+            }
+        }
+        false
+    }
+
+    #[inline]
+    fn is_modifire_key(keycode: &VirtualKeyCode) -> bool {
+        match *keycode {
+            VirtualKeyCode::LControl
+            | VirtualKeyCode::RControl
+            | VirtualKeyCode::LAlt
+            | VirtualKeyCode::RAlt
+            | VirtualKeyCode::LShift
+            | VirtualKeyCode::RShift => true,
+            _ => false,
         }
     }
 }
