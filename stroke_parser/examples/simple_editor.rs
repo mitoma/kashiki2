be86b439ce461f543bf6e3e97edd47e374d0d079
mitoma <mutetheradio@gmail.com>
2020-05-03 11:22:21 +0900

    fix moromoro

--- stroke_parser/examples/simple_editor.rs
+++ stroke_parser/examples/simple_editor.rs
@@ -1,6 +1,4 @@
-use stroke_parser::{
-    action_store_parser, keys, Action, ActionStore, KeyBind, KeyWithModifier, Stroke,
-};
+use stroke_parser::{action_store_parser, Action, ActionStore};
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
@@ -13,7 +11,6 @@ fn main() {
 
     let mut caret = text_buffer::caret::Caret::new(0, 0);
     let mut text_buffer = text_buffer::buffer::Buffer::new();
-    text_buffer.insert_string(&mut caret, "hello world".to_string());
 
     let mut store: ActionStore = Default::default();
     let key_setting = include_str!("key-settings.txt");
@@ -28,15 +25,45 @@ fn main() {
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
         match store.winit_event_to_action(&event) {
+            Some(Action::Command(category, name)) if *category == "system" => {
+                match &*name.to_string() {
+                    "exit" => *control_flow = ControlFlow::Exit,
+                    "return" => text_buffer.insert_enter(&mut caret),
+                    "backspace" => {
+                        text_buffer.backspace(&mut caret);
+                    }
+                    "delete" => {
+                        text_buffer.delete(&mut caret);
+                    }
+                    "previous" => {
+                        text_buffer.previous(&mut caret);
+                    }
+                    "next" => {
+                        text_buffer.next(&mut caret);
+                    }
+                    "back" => {
+                        text_buffer.back(&mut caret);
+                    }
+                    "forward" => {
+                        text_buffer.forward(&mut caret);
+                    }
+                    _ => (),
+                }
+            }
+
             Some(Action::Command(category, name)) if *category == "system" && *name == "exit" => {
                 *control_flow = ControlFlow::Exit
             }
             Some(Action::Command(category, name)) if *category == "system" && *name == "return" => {
                 text_buffer.insert_enter(&mut caret);
             }
+            Some(Action::Command(category, name))
+                if *category == "system" && *name == "backspace" =>
+            {
+                text_buffer.backspace(&mut caret);
+            }
             Some(Action::Keytype(c)) => {
                 text_buffer.insert_char(&mut caret, c);
-                println!("{}", text_buffer.to_buffer_string());
             }
             Some(command) => println!("{:?}", command),
             None => {}
@@ -49,5 +76,7 @@ fn main() {
             } if window_id == window.id() => *control_flow = ControlFlow::Exit,
             _ => (),
         }
+
+        println!("\n----\n{}", text_buffer.to_buffer_string());
     });
 }
