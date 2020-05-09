30ff9af2663c751e035fd434e8c285113fe5d70c
mitoma <mutetheradio@gmail.com>
2020-05-09 13:49:40 +0900

    勉強用のサンプルコードを追加

---
+++ wgpu_gui/examples/framework.rs
@@ -0,0 +1,185 @@
+use winit::{
+    event::{self, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
+
+#[cfg_attr(rustfmt, rustfmt_skip)]
+#[allow(unused)]
+pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, 0.5, 0.0,
+    0.0, 0.0, 0.5, 1.0,
+);
+
+#[allow(dead_code)]
+pub fn cast_slice<T>(data: &[T]) -> &[u8] {
+    use std::mem::size_of;
+    use std::slice::from_raw_parts;
+
+    unsafe { from_raw_parts(data.as_ptr() as *const u8, data.len() * size_of::<T>()) }
+}
+
+#[allow(dead_code)]
+pub enum ShaderStage {
+    Vertex,
+    Fragment,
+    Compute,
+}
+
+pub trait Example: 'static + Sized {
+    fn init(
+        sc_desc: &wgpu::SwapChainDescriptor,
+        device: &wgpu::Device,
+    ) -> (Self, Option<wgpu::CommandBuffer>);
+    fn resize(
+        &mut self,
+        sc_desc: &wgpu::SwapChainDescriptor,
+        device: &wgpu::Device,
+    ) -> Option<wgpu::CommandBuffer>;
+    fn update(&mut self, event: WindowEvent);
+    fn render(
+        &mut self,
+        frame: &wgpu::SwapChainOutput,
+        device: &wgpu::Device,
+    ) -> wgpu::CommandBuffer;
+}
+
+async fn run_async<E: Example>(event_loop: EventLoop<()>, window: Window) {
+    log::info!("Initializing the surface...");
+
+    let (size, surface) = {
+        let size = window.inner_size();
+        let surface = wgpu::Surface::create(&window);
+        (size, surface)
+    };
+
+    let adapter = wgpu::Adapter::request(
+        &wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::Default,
+            compatible_surface: Some(&surface),
+        },
+        wgpu::BackendBit::PRIMARY,
+    )
+    .await
+    .unwrap();
+
+    let (device, queue) = adapter
+        .request_device(&wgpu::DeviceDescriptor {
+            extensions: wgpu::Extensions {
+                anisotropic_filtering: false,
+            },
+            limits: wgpu::Limits::default(),
+        })
+        .await;
+
+    let mut sc_desc = wgpu::SwapChainDescriptor {
+        usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+        // TODO: Allow srgb unconditionally
+        format: if cfg!(target_arch = "wasm32") {
+            wgpu::TextureFormat::Bgra8Unorm
+        } else {
+            wgpu::TextureFormat::Bgra8UnormSrgb
+        },
+        width: size.width,
+        height: size.height,
+        present_mode: wgpu::PresentMode::Mailbox,
+    };
+    let mut swap_chain = device.create_swap_chain(&surface, &sc_desc);
+
+    log::info!("Initializing the example...");
+    let (mut example, init_command_buf) = E::init(&sc_desc, &device);
+    if let Some(command_buf) = init_command_buf {
+        queue.submit(&[command_buf]);
+    }
+
+    log::info!("Entering render loop...");
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = if cfg!(feature = "metal-auto-capture") {
+            ControlFlow::Exit
+        } else {
+            ControlFlow::Poll
+        };
+        match event {
+            event::Event::MainEventsCleared => window.request_redraw(),
+            event::Event::WindowEvent {
+                event: WindowEvent::Resized(size),
+                ..
+            } => {
+                log::info!("Resizing to {:?}", size);
+                sc_desc.width = size.width;
+                sc_desc.height = size.height;
+                swap_chain = device.create_swap_chain(&surface, &sc_desc);
+                let command_buf = example.resize(&sc_desc, &device);
+                if let Some(command_buf) = command_buf {
+                    queue.submit(&[command_buf]);
+                }
+            }
+            event::Event::WindowEvent { event, .. } => match event {
+                WindowEvent::KeyboardInput {
+                    input:
+                        event::KeyboardInput {
+                            virtual_keycode: Some(event::VirtualKeyCode::Escape),
+                            state: event::ElementState::Pressed,
+                            ..
+                        },
+                    ..
+                }
+                | WindowEvent::CloseRequested => {
+                    *control_flow = ControlFlow::Exit;
+                }
+                _ => {
+                    example.update(event);
+                }
+            },
+            event::Event::RedrawRequested(_) => {
+                let frame = swap_chain
+                    .get_next_texture()
+                    .expect("Timeout when acquiring next swap chain texture");
+                let command_buf = example.render(&frame, &device);
+                queue.submit(&[command_buf]);
+            }
+            _ => {}
+        }
+    });
+}
+
+pub fn run<E: Example>(title: &str) {
+    let event_loop = EventLoop::new();
+    let mut builder = winit::window::WindowBuilder::new();
+    builder = builder.with_title(title);
+    #[cfg(windows_OFF)] //TODO
+    {
+        use winit::platform::windows::WindowBuilderExtWindows;
+        builder = builder.with_no_redirection_bitmap(true);
+    }
+    let window = builder.build(&event_loop).unwrap();
+
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        env_logger::init();
+        futures::executor::block_on(run_async::<E>(event_loop, window));
+    }
+    #[cfg(target_arch = "wasm32")]
+    {
+        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+        console_log::init().expect("could not initialize logger");
+        use winit::platform::web::WindowExtWebSys;
+        // On wasm, append the canvas to the document body
+        web_sys::window()
+            .and_then(|win| win.document())
+            .and_then(|doc| doc.body())
+            .and_then(|body| {
+                body.append_child(&web_sys::Element::from(window.canvas()))
+                    .ok()
+            })
+            .expect("couldn't append canvas to document body");
+        wasm_bindgen_futures::spawn_local(run_async::<E>(event_loop, window));
+    }
+}
+
+// This allows treating the framework as a standalone example,
+// thus avoiding listing the example names in `Cargo.toml`.
+#[allow(dead_code)]
+fn main() {}
--- wgpu_gui/src/main.rs
+++ wgpu_gui/src/main.rs
@@ -1,14 +1,59 @@
+use rusttype::gpu_cache::Cache;
+use rusttype::{point, vector, Font, PositionedGlyph, Rect, Scale};
+use std::borrow::Cow;
+use std::env;
+use std::error::Error;
 use stroke_parser::{action_store_parser, Action, ActionStore};
-use wgpu_glyph::{GlyphBrushBuilder, Scale, Section};
+
+fn layout_paragraph<'a>(
+    font: &Font<'a>,
+    scale: Scale,
+    width: u32,
+    text: &str,
+) -> Vec<PositionedGlyph<'a>> {
+    let mut result = Vec::new();
+    let v_metrics = font.v_metrics(scale);
+    let advance_height = v_metrics.ascent - v_metrics.descent + v_metrics.line_gap;
+    let mut caret = point(0.0, v_metrics.ascent);
+    let mut last_glyph_id = None;
+    for c in text.chars() {
+        if c.is_control() {
+            match c {
+                '\r' => {
+                    caret = point(0.0, caret.y + advance_height);
+                }
+                '\n' => {}
+                _ => {}
+            }
+            continue;
+        }
+        let base_glyph = font.glyph(c);
+        if let Some(id) = last_glyph_id.take() {
+            caret.x += font.pair_kerning(scale, id, base_glyph.id());
+        }
+        last_glyph_id = Some(base_glyph.id());
+        let mut glyph = base_glyph.scaled(scale).positioned(caret);
+        if let Some(bb) = glyph.pixel_bounding_box() {
+            if bb.max.x > width as i32 {
+                caret = point(0.0, caret.y + advance_height);
+                glyph.set_position(caret);
+                last_glyph_id = None;
+            }
+        }
+        caret.x += glyph.unpositioned().h_metrics().advance_width;
+        result.push(glyph);
+    }
+    result
+}
 
 fn main() -> Result<(), String> {
     env_logger::init();
 
+    // setup text editor buffer
     let mut caret = text_buffer::caret::Caret::new(0, 0);
     let mut text_buffer = text_buffer::buffer::Buffer::new();
     let mut store: ActionStore = Default::default();
     let key_setting = include_str!("key-settings.txt");
-    println!("{}", key_setting);
     let keybinds = action_store_parser::parse_setting(String::from(key_setting));
     keybinds
         .iter()
@@ -61,11 +106,16 @@ fn main() -> Result<(), String> {
         },
     );
 
-    // Prepare glyph_brush
+    // Prepare font.
     let inconsolata: &[u8] = include_bytes!("TakaoGothic.ttf");
-    let mut glyph_brush = GlyphBrushBuilder::using_font_bytes(inconsolata)
-        .expect("Load fonts")
-        .build(&device, render_format);
+    let font = Font::from_bytes(inconsolata as &[u8]).unwrap();
+
+    let scale = window.scale_factor();
+
+    let (cache_width, cache_height) = ((1024.0 * scale) as u32, (1024.0 * scale) as u32);
+    let mut cache: Cache<'static> = Cache::builder()
+        .dimensions(cache_width, cache_height)
+        .build();
 
     // Render loop
     window.request_redraw();
@@ -104,6 +154,11 @@ fn main() -> Result<(), String> {
             }
             Some(Action::Keytype(c)) => {
                 text_buffer.insert_char(&mut caret, c);
+                let glyph = font
+                    .glyph(c)
+                    .scaled(Scale::uniform(32.0))
+                    .positioned(point(0.0, 0.0));
+                cache.queue_glyph(0, glyph); // font_id is always 0. because i use single font.
                 window.request_redraw();
             }
             Some(command) => println!("{:?}", command),
@@ -138,6 +193,48 @@ fn main() -> Result<(), String> {
                     label: Some("Redraw"),
                 });
 
+                let create_glyph_texture_result = cache.cache_queued(|rect, data| {
+                    let texture_extent = wgpu::Extent3d {
+                        width: rect.width(),
+                        height: rect.height(),
+                        depth: 1,
+                    };
+                    let texture = device.create_texture(&wgpu::TextureDescriptor {
+                        size: texture_extent,
+                        array_layer_count: 1,
+                        mip_level_count: 1,
+                        sample_count: 1,
+                        dimension: wgpu::TextureDimension::D2,
+                        format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                        usage: wgpu::TextureUsage::SAMPLED | wgpu::TextureUsage::COPY_DST,
+                        label: None,
+                    });
+                    let texture_view = texture.create_default_view();
+                    let temp_buf =
+                        device.create_buffer_with_data(data, wgpu::BufferUsage::COPY_SRC);
+                    encoder.copy_buffer_to_texture(
+                        wgpu::BufferCopyView {
+                            buffer: &temp_buf,
+                            offset: 0,
+                            bytes_per_row: 4 * rect.width(),
+                            rows_per_image: 0,
+                        },
+                        wgpu::TextureCopyView {
+                            texture: &texture,
+                            mip_level: 0,
+                            array_layer: 0,
+                            origin: wgpu::Origin3d::ZERO,
+                        },
+                        texture_extent,
+                    );
+                    println!("create texture.");
+                });
+
+                match create_glyph_texture_result {
+                    Ok(_) => (),
+                    Err(e) => println!("{}", e),
+                }
+
                 // Get the next frame
                 let frame = swap_chain.get_next_texture().expect("Get next frame");
 
@@ -160,21 +257,8 @@ fn main() -> Result<(), String> {
                     });
                 }
 
-                glyph_brush.queue(Section {
-                    text: &text_buffer.to_buffer_string(),
-                    screen_position: (30.0, 30.0),
-                    color: [0.0, 0.0, 0.0, 1.0],
-                    scale: Scale { x: 40.0, y: 40.0 },
-                    bounds: (size.width as f32, size.height as f32),
-                    ..Section::default()
-                });
-
-                // Draw the text!
-                glyph_brush
-                    .draw_queued(&device, &mut encoder, &frame.view, size.width, size.height)
-                    .expect("Draw queued");
-
-                queue.submit(&[encoder.finish()]);
+                // ここから書いていく処理が必要なはず。
+                encoder.finish();
             }
             _ => {
                 *control_flow = winit::event_loop::ControlFlow::Wait;
