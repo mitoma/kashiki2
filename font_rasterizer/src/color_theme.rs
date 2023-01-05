0adf1b3082ccdcd004e133c9f9acf2266027b41c
mitoma <mutetheradio@gmail.com>
2023-01-05 15:05:18 +0900

    move theme

--- font_rasterizer/src/color_theme.rs
+++ font_rasterizer/src/color_theme.rs
@@ -107,3 +107,79 @@ impl SolarizedColor {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+
+    // good color scheme.
+    // https://ethanschoonover.com/solarized/
+    const SCHEMES: [(&str, u32, u32, u32); 16] = [
+        ("base03", 10, 43, 54),
+        ("base02", 19, 54, 66),
+        ("base01", 88, 110, 117),
+        ("base00", 101, 123, 131),
+        ("base0", 131, 148, 150),
+        ("base1", 147, 161, 161),
+        ("base2", 238, 232, 213),
+        ("base3", 253, 246, 227),
+        ("yellow", 181, 137, 0),
+        ("orange", 203, 75, 22),
+        ("red", 220, 50, 47),
+        ("magenta", 211, 54, 130),
+        ("violet", 108, 113, 196),
+        ("blue", 38, 139, 210),
+        ("cyan", 42, 161, 152),
+        ("green", 133, 153, 0),
+    ];
+
+    #[test]
+    fn generate_color_table_for_wgsl() {
+        SCHEMES.iter().for_each(|scheme| {
+            println!(
+                "let {:10} = vec4<f32>({:.10}, {:.10}, {:.10}, 1.0);",
+                scheme.0,
+                linear_to_srgb(scheme.1),
+                linear_to_srgb(scheme.2),
+                linear_to_srgb(scheme.3)
+            );
+        });
+    }
+
+    #[test]
+    fn generate_color_table_for_rust_enum() {
+        println!("pub(crate) enum SolarizedColor {{");
+        SCHEMES.iter().for_each(|scheme| {
+            println!(
+                "{:10}({:.10}, {:.10}, {:.10}),",
+                scheme.0.to_uppercase(),
+                linear_to_srgb(scheme.1),
+                linear_to_srgb(scheme.2),
+                linear_to_srgb(scheme.3)
+            );
+        });
+        println!("}};");
+
+        SCHEMES.iter().for_each(|scheme| {
+            println!(
+                "SolarizedColor::{:10} => [{:.10}, {:.10}, {:.10}],",
+                scheme.0.to_uppercase(),
+                linear_to_srgb(scheme.1),
+                linear_to_srgb(scheme.2),
+                linear_to_srgb(scheme.3)
+            );
+        });
+        println!("}};");
+    }
+
+    // こちらの記事を参考に linear の RGB 情報を sRGB に変換
+    // http://www.psy.ritsumei.ac.jp/~akitaoka/RGBtoXYZ_etal01.html
+    // https://en.wikipedia.org/wiki/SRGB
+    fn linear_to_srgb(value: u32) -> f32 {
+        let value: f32 = value as f32 / 255.0;
+        if value <= 0.04045 {
+            value / 12.92
+        } else {
+            ((value + 0.055) / 1.055).powf(2.4)
+        }
+    }
+}
