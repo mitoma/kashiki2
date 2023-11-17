931bb7e8c0f17f0dfa11c6b9ea772f607ee4561a
mitoma <mutetheradio@gmail.com>
2023-11-18 07:42:53 +0900

    collector にした

--- font_collector/src/lib.rs
+++ font_collector/src/lib.rs
@@ -4,14 +4,76 @@ use std::path::PathBuf;
 
 use convert_text::PreferredLanguage;
 
-fn font_dir() -> PathBuf {
+pub struct FontCollector {
+    font_paths: Vec<PathBuf>,
+    preffered_language: Option<PreferredLanguage>,
+}
+
+impl FontCollector {
+    pub fn new() -> Self {
+        Self {
+            font_paths: Vec::new(),
+            preffered_language: Some(PreferredLanguage::Japanese),
+        }
+    }
+
+    pub fn add_system_fonts(&mut self) {
+        self.font_paths.push(system_font_dir());
+    }
+
+    pub fn add_font_path(&mut self, path: PathBuf) {
+        self.font_paths.push(path);
+    }
+
+    fn list_font_files(&self) -> Vec<PathBuf> {
+        let mut fonts = Vec::new();
+
+        self.font_paths.iter().for_each(|font_path| {
+            if font_path.is_file() {
+                fonts.push(font_path.clone());
+                return;
+            }
+            for entry in std::fs::read_dir(font_path).unwrap() {
+                let entry = entry.unwrap();
+                let path = entry.path();
+                if path.is_file() {
+                    fonts.push(path);
+                }
+            }
+        });
+        fonts
+    }
+
+    pub fn list_fonts(&self) -> Vec<Font> {
+        let font_file_paths = self.list_font_files();
+        let mut fonts = Vec::new();
+        for font_path in font_file_paths {
+            let font_names = font_names(&font_path, self.preffered_language);
+            if font_names.is_empty() {
+                continue;
+            }
+            fonts.push(Font::File(font_path.clone(), font_names));
+        }
+        fonts
+    }
+
+    pub fn load_font(&self, font_name: &str) -> Option<Vec<u8>> {
+        self.list_fonts()
+            .iter()
+            .filter(|f| f.contains(font_name))
+            .map(|f| f.data())
+            .next()
+    }
+}
+
+fn system_font_dir() -> PathBuf {
     let buf = PathBuf::from("C:\\Windows\\Fonts");
     buf
 }
 
 fn list_font_files() -> Vec<PathBuf> {
     let mut fonts = Vec::new();
-    let font_dir = font_dir();
+    let font_dir = system_font_dir();
     for entry in std::fs::read_dir(font_dir).unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
@@ -97,5 +159,40 @@ mod tests {
         font_names.iter().for_each(|name| {
             println!("font_name:{:?}", name,);
         });
+    }
+
+    #[test]
+    fn test_list_fonts() {
+        let mut collector = FontCollector::new();
+        collector.add_system_fonts();
+        let fonts = collector.list_fonts();
+
+        let mut font_names = fonts
+            .into_iter()
+            .flat_map(|font| match font {
+                Font::File(_path, names) => names,
+                Font::InMemory(_id, names) => names,
+            })
+            .collect::<Vec<String>>();
+        font_names.sort();
+        font_names.iter().for_each(|name| {
+            println!("font_name:{:?}", name,);
+        });
+    }
+
+    #[test]
+    fn test_list_fonts2() {
+        let mut collector = FontCollector::new();
+        collector.add_font_path(PathBuf::from("../font_rasterizer/examples/font"));
+        let fonts = collector.list_fonts();
+
+        let mut font_names = fonts
+            .into_iter()
+            .flat_map(|font| match font {
+                Font::File(_path, names) => names,
+                Font::InMemory(_id, names) => names,
+            })
+            .collect::<Vec<String>>();
+        assert_eq!(font_names.len(), 2);
     }
 }
