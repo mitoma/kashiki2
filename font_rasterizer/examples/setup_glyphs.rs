2f5b65458d6a1a09853cde4cdae221997a90d76c
mitoma <mutetheradio@gmail.com>
2024-03-12 08:21:00 +0900

    グリフ生成のパフォーマンスを見るための example を追加

---
+++ font_rasterizer/examples/setup_glyphs.rs
@@ -0,0 +1,106 @@
+use std::collections::HashSet;
+
+use font_collector::FontCollector;
+use font_rasterizer::{
+    camera::Camera,
+    color_theme::ColorTheme,
+    font_buffer::GlyphVertexBuffer,
+    instances::GlyphInstances,
+    rasterizer_pipeline::Quarity,
+    support::{run_support, Flags, InputResult, SimpleStateCallback, SimpleStateSupport},
+};
+use instant::Instant;
+use winit::event::WindowEvent;
+
+const FONT_DATA: &[u8] = include_bytes!("font/HackGenConsole-Regular.ttf");
+const EMOJI_FONT_DATA: &[u8] = include_bytes!("font/NotoEmoji-Regular.ttf");
+
+pub fn main() {
+    std::env::set_var("RUST_LOG", "support_test=debug");
+    pollster::block_on(run());
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+pub async fn run() {
+    let mut collector = FontCollector::default();
+    collector.add_system_fonts();
+    let data = collector.load_font("UD デジタル 教科書体 N-R");
+
+    let font_binaries = vec![
+        data.unwrap(),
+        collector
+            .convert_font(EMOJI_FONT_DATA.to_vec(), None)
+            .unwrap(),
+    ];
+
+    let callback = SingleCharCallback {
+        camera: Camera::basic((800, 600)),
+    };
+    let support = SimpleStateSupport {
+        window_icon: None,
+        window_title: "Hello".to_string(),
+        window_size: (800, 600),
+        callback: Box::new(callback),
+        quarity: Quarity::VeryHigh,
+        color_theme: ColorTheme::SolarizedDark,
+        flags: Flags::DEFAULT,
+        font_binaries,
+    };
+    run_support(support).await;
+}
+
+struct SingleCharCallback {
+    camera: Camera,
+}
+
+impl SimpleStateCallback for SingleCharCallback {
+    fn init(
+        &mut self,
+        glyph_vertex_buffer: &mut GlyphVertexBuffer,
+        _color_theme: &ColorTheme,
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+    ) {
+        let start = Instant::now();
+        let _char_ranges = [
+            'a'..='z',
+            'A'..='Z',
+            '0'..='9',
+            'あ'..='ん',
+            'ア'..='ン',
+            '亜'..='腺',
+            '一'..='龠',
+            '㐀'..='䶿',
+        ]
+        .iter()
+        .map(|char_range| char_range.clone().into_iter().collect::<HashSet<char>>())
+        .for_each(|c| {
+            let start = Instant::now();
+            println!("c len: {}", c.len());
+            glyph_vertex_buffer.append_glyph(device, queue, c).unwrap();
+            let end = Instant::now();
+            println!("init: {:?}", end - start);
+        });
+        let end = Instant::now();
+        println!("init: {:?}", end - start);
+    }
+
+    fn update(
+        &mut self,
+        _glyph_vertex_buffer: &mut GlyphVertexBuffer,
+        _color_theme: &ColorTheme,
+        _device: &wgpu::Device,
+        _queue: &wgpu::Queue,
+    ) {
+    }
+
+    fn input(&mut self, _event: &WindowEvent) -> InputResult {
+        InputResult::Noop
+    }
+
+    fn resize(&mut self, _width: u32, _height: u32) {}
+
+    fn render(&mut self) -> (&Camera, Vec<&GlyphInstances>) {
+        (&self.camera, Vec::new())
+    }
+}
