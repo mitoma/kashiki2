547c567678abb687003268d81a268923b0693e1e
mitoma <mutetheradio@gmail.com>
2024-09-28 11:02:06 +0900

    そろそろ DOS 窓出さないようにする

--- kashikishi/src/main.rs
+++ kashikishi/src/main.rs
@@ -1,4 +1,4 @@
-/*#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]*/
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 /* ↑は Windows で実行する時にコマンドプロンプトが開かないようにするためのもの。 */
 mod action_repository;
 mod categorized_memos;
