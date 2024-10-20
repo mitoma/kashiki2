f71e0bd5508dc2d89480bb863224597957f8101e
mitoma <mutetheradio@gmail.com>
2024-10-20 10:15:46 +0900

    import を楽に書けるように直すなど

--- site/src/custom_js/showcase.js
+++ site/src/custom_js/showcase.js
@@ -1,26 +1,26 @@
-import init, { send_log, toggle_direction, look_current_and_centering, change_theme_dark, change_theme_light, zoom_in, zoom_out, toggle_psychedelic } from "../wasm/showcase/showcase.js";
+import init, * as kashiki from "../wasm/showcase/showcase.js";
 init().then(() => {
     console.log("WASM Loaded");
-    send_log("Hello from JS");
+    kashiki.send_log("Hello from JS");
     document.getElementById("toggle-direction-button").addEventListener("click", () => {
-        toggle_direction();
+        kashiki.toggle_direction();
     });
     document.getElementById("centering-button").addEventListener("click", () => {
-        look_current_and_centering();
+        kashiki.look_current_and_centering();
     });
     document.getElementById("dark-mode-button").addEventListener("click", () => {
-        change_theme_dark();
+        kashiki.change_theme_dark();
     });
     document.getElementById("light-mode-button").addEventListener("click", () => {
-        change_theme_light();
+        kashiki.change_theme_light();
     });
     document.getElementById("zoom-in-button").addEventListener("click", () => {
-        zoom_in();
+        kashiki.zoom_in();
     });
     document.getElementById("zoom-out-button").addEventListener("click", () => {
-        zoom_out();
+        kashiki.zoom_out();
     });
     document.getElementById("psychedelic-mode-button").addEventListener("click", () => {
-        toggle_psychedelic();
+        kashiki.toggle_psychedelic();
     });
 });
