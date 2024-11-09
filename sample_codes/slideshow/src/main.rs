b7940f43a05992ff466e23531111228d9496b0a3
mitoma <mutetheradio@gmail.com>
2024-11-09 16:32:17 +0900

    delete slideshow

--- sample_codes/slideshow/src/main.rs
+++
@@ -1,8 +0,0 @@
-use slideshow::run;
-
-pub fn main() {
-    std::env::set_var("RUST_LOG", "slideshow=info");
-    //std::env::set_var("FONT_RASTERIZER_DEBUG", "debug");
-    env_logger::init();
-    pollster::block_on(run());
-}
