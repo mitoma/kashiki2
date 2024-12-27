2f748dc268b8e0430da8120e0d595b0fba302525
mitoma <mutetheradio@gmail.com>
2024-12-28 00:45:35 +0900

    anyhow への依存が全部消えてしまった。意外。

--- rokid_3dof/examples/sensor_test.rs
+++ rokid_3dof/examples/sensor_test.rs
@@ -3,9 +3,9 @@ use std::{
     time::Duration,
 };
 
-use rokid_3dof::RokidMax;
+use rokid_3dof::{RokidMax, RokidMaxError};
 
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<(), RokidMaxError> {
     let rokid_max = RokidMax::new()?;
     loop {
         println!("{:?}", rokid_max.quaternion());
