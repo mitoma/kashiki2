3c66186173f4d3393ef28514256d07640772c0c1
mitoma <mutetheradio@gmail.com>
2025-02-15 18:06:51 +0900

    初期段階を修正

--- ttf-overlap-remover/src/lib.rs
+++ ttf-overlap-remover/src/lib.rs
@@ -1,42 +1,27 @@
 use tiny_skia::Point;
 
 enum PathSegment {
-    Line { start: Point, end: Point },
-    Quadratic(Point, Point, Point),
-    Cubic(Point, Point, Point, Point),
+    Line {
+        from: Point,
+        to: Point,
+    },
+    Quadratic {
+        from: Point,
+        to: Point,
+        control: Point,
+    },
+    Cubic {
+        from: Point,
+        to: Point,
+        control1: Point,
+        control2: Point,
+    },
 }
 
 fn cross_point(a: PathSegment, b: PathSegment) -> Vec<Point> {
     match (a, b) {
-        (
-            PathSegment::Line {
-                start: p1_start,
-                end: p1_end,
-            },
-            PathSegment::Line {
-                start: p2_start,
-                end: p2_end,
-            },
-        ) => {
-            // 直線同士の交点を求める
-            let denom = (p2_end.y - p2_start.y) * (p1_end.x - p1_start.x)
-                - (p2_end.x - p2_start.x) * (p1_end.y - p1_start.y);
-            if denom == 0.0 {
-                return vec![]; // 平行な場合は交点なし
-            }
-            let ua = ((p2_end.x - p2_start.x) * (p1_start.y - p2_start.y)
-                - (p2_end.y - p2_start.y) * (p1_start.x - p2_start.x))
-                / denom;
-            let ub = ((p1_end.x - p1_start.x) * (p1_start.y - p2_start.y)
-                - (p1_end.y - p1_start.y) * (p1_start.x - p2_start.x))
-                / denom;
-            if ua >= 0.0 && ua <= 1.0 && ub >= 0.0 && ub <= 1.0 {
-                let x = p1_start.x + ua * (p1_end.x - p1_start.x);
-                let y = p1_start.y + ua * (p1_end.y - p1_start.y);
-                vec![Point::from_xy(x, y)]
-            } else {
-                vec![] // 線分上に交点がない場合
-            }
+        (PathSegment::Line { .. }, PathSegment::Line { .. }) => {
+            todo!()
         }
         // 他のセグメントの組み合わせについては未実装
         _ => vec![],
@@ -56,8 +41,8 @@ mod tests {
         let p3 = Point::from_xy(0.0, 2.0);
         let p4 = Point::from_xy(2.0, 0.0);
 
-        let segment1 = PathSegment::Line { start: p1, end: p2 };
-        let segment2 = PathSegment::Line { start: p3, end: p4 };
+        let segment1 = PathSegment::Line { from: p1, to: p2 };
+        let segment2 = PathSegment::Line { from: p3, to: p4 };
 
         let result = cross_point(segment1, segment2);
         assert_eq!(result.len(), 1);
@@ -71,8 +56,8 @@ mod tests {
         let p3 = Point::from_xy(0.0, 1.0);
         let p4 = Point::from_xy(2.0, 3.0);
 
-        let segment1 = PathSegment::Line { start: p1, end: p2 };
-        let segment2 = PathSegment::Line { start: p3, end: p4 };
+        let segment1 = PathSegment::Line { from: p1, to: p2 };
+        let segment2 = PathSegment::Line { from: p3, to: p4 };
 
         let result = cross_point(segment1, segment2);
         assert_eq!(result.len(), 0);
@@ -85,8 +70,8 @@ mod tests {
         let p3 = Point::from_xy(2.0, 2.0);
         let p4 = Point::from_xy(3.0, 3.0);
 
-        let segment1 = PathSegment::Line { start: p1, end: p2 };
-        let segment2 = PathSegment::Line { start: p3, end: p4 };
+        let segment1 = PathSegment::Line { from: p1, to: p2 };
+        let segment2 = PathSegment::Line { from: p3, to: p4 };
 
         let result = cross_point(segment1, segment2);
         assert_eq!(result.len(), 0);
