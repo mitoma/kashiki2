7299255e5ae2eecf9cb742365946af4ac2c2bb63
mitoma <mutetheradio@gmail.com>
2025-07-05 18:43:36 +0900

    不要なコードを削除したり

--- font_rasterizer/src/overlap_record_texture.rs
+++ font_rasterizer/src/overlap_record_texture.rs
@@ -1,39 +1,3 @@
-pub struct OverlapRecordTexture {
-    pub texture: wgpu::Texture,
-    pub view: wgpu::TextureView,
-    pub texture_format: wgpu::TextureFormat,
-}
-
-impl OverlapRecordTexture {
-    pub fn new(device: &wgpu::Device, size: (u32, u32), label: Option<&str>) -> Self {
-        let texture_format = wgpu::TextureFormat::Rgba32Uint;
-        let size = wgpu::Extent3d {
-            width: size.0,
-            height: size.1,
-            depth_or_array_layers: 1,
-        };
-
-        let texture = device.create_texture(&wgpu::TextureDescriptor {
-            label,
-            size,
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format: texture_format,
-            usage: wgpu::TextureUsages::STORAGE_BINDING | wgpu::TextureUsages::RENDER_ATTACHMENT,
-            view_formats: &[],
-        });
-
-        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
-
-        Self {
-            texture,
-            view,
-            texture_format,
-        }
-    }
-}
-
 pub struct OverlapRecordBuffer {
     pub buffer: wgpu::Buffer,
 }
