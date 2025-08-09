ccb378afb13554873d57849fec338b8b81819b4a
mitoma <mutetheradio@gmail.com>
2025-08-09 17:22:55 +0900

    スクリプトにコメントを追加

--- dev-sites.sh
+++ dev-sites.sh
@@ -4,8 +4,11 @@ OUT_DIR="${GIT_ROOT_DIR}/site/src/wasm/showcase"
 
 cd "${GIT_ROOT_DIR}/sample_codes/showcase"
 rm -rf "${OUT_DIR}"
+echo "cargo build"
 cargo build --target wasm32-unknown-unknown --release
+echo "wasm-bindgen"
 wasm-bindgen "${GIT_ROOT_DIR}/target/wasm32-unknown-unknown/release/showcase.wasm" --target web --out-dir "${OUT_DIR}"
+echo "wasm-opt"
 wasm-opt -Oz -o "${OUT_DIR}/showcase_bg.wasm.out" "${OUT_DIR}/showcase_bg.wasm"
 rm "${OUT_DIR}/showcase_bg.wasm"
 mv "${OUT_DIR}/showcase_bg.wasm.out" "${OUT_DIR}/showcase_bg.wasm"
