df8dc2a64dd2ac94344dc2a8e0572d0a51b37c59
mitoma <mutetheradio@gmail.com>
2025-08-12 20:21:23 +0900

    update fmt

--- font_rasterizer/src/debug_mode.rs
+++ font_rasterizer/src/debug_mode.rs
@@ -5,13 +5,11 @@ pub(crate) struct DebugFlags {
     pub(crate) debug_shader: bool,
 }
 
-pub static DEBUG_FLAGS: LazyLock<DebugFlags> = LazyLock::new(|| {
-    DebugFlags {
-        show_glyph_outline: std::env::var("FONT_RASTERIZER_DEBUG")
-            .map(|_debug| true)
-            .unwrap_or(false),
-        debug_shader: std::env::var("FONT_RASTERIZER_DEBUG_SHADER")
-            .map(|_debug| true)
-            .unwrap_or(false),
-    }
+pub static DEBUG_FLAGS: LazyLock<DebugFlags> = LazyLock::new(|| DebugFlags {
+    show_glyph_outline: std::env::var("FONT_RASTERIZER_DEBUG")
+        .map(|_debug| true)
+        .unwrap_or(false),
+    debug_shader: std::env::var("FONT_RASTERIZER_DEBUG_SHADER")
+        .map(|_debug| true)
+        .unwrap_or(false),
 });
